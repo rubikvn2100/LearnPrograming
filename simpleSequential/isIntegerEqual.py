3df575cdc0b3e0cb115f83f7ccb16eddf28b4b73
Khoa Le <rubikvn2100@gmail.com>
2023-02-20 22:57:48 -0800

    Add isIntegerEqual.py problem statement.

---
+++ simpleSequential/isIntegerEqual.py
@@ -0,0 +1,2 @@
+# Create a function that take two integer numbers and return True if they are equal, otherwise return False.
+# This function is simple and you will not need to use condition structure.
