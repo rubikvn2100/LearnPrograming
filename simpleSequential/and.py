254515c65546333bc52fb0bef29329c93d5f1cea
Khoa Le <rubikvn2100@gmail.com>
2023-02-21 20:30:59 -0800

    Add and.py problem statement.

---
+++ simpleSequential/and.py
@@ -0,0 +1,5 @@
+# Create a function that take two boolean values, "and" the values together and return the result.
+# This function is simple and you will not need to use condition structure.
+# Ex: 
+#   and(True, False) return False
+#   and(True, True)  return True
