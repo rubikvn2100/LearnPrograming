d1532020858ea8f95d8f32e68e84defc08a3f553
Dat Nguyen <tiendatlg1234@gmail.com>
2023-02-22 09:23:57 +0700

    Fix typo in double.test.py file.

--- simpleSequential/double.test.py
+++ simpleSequential/double.test.py
@@ -5,10 +5,10 @@ class TestDoubleFunction(unittest.TestCase):
     def test_doubleFunctionCase1(self):
         self.assertEqual(double(5), 10, "Error: double result is incorrect.")
 
-    def test_DoubleFunctionCase2(self):
+    def test_doubleFunctionCase2(self):
         self.assertEqual(double(-3), -6, "Error: double result is incorrect.")
     
-    def test_DoubleFunctionCase3(self):
+    def test_doubleFunctionCase3(self):
         self.assertEqual(double(0), 0, "Error: double result is incorrect.")
 
 if __name__ == "__main__":
