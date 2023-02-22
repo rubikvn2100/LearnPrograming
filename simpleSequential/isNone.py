289efb9b17efb6835ef377802d4abccdc2206e26
Khoa Le <rubikvn2100@gmail.com>
2023-02-21 20:31:48 -0800

    Add isNone.py problem statement.

---
+++ simpleSequential/isNone.py
@@ -0,0 +1,7 @@
+# Create a function that take one variable and return True if it is None, otherwise return False.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   isNone(None)    return True
+#   isNone("abcde") return False
+#   isNone(True)    return False
+#   isNone(5)       return False
