27ea1aa949b3cf9ac6e53e988238626d9161073e
Khoa Le <rubikvn2100@gmail.com>
2023-02-21 20:32:16 -0800

    Add or.py problem statement.

---
+++ simpleSequential/or.py
@@ -0,0 +1,5 @@
+# Create a function that take two boolean values, "or" the values together and return the result.
+# This function is simple and you will not need to use condition structure.
+# Ex: 
+#   or(False, False) return False
+#   or(False, True)  return True
