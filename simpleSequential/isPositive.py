6ad6af9435f3db6f71f4dd7c9556c67e74fbbc7d
Khoa Le <rubikvn2100@gmail.com>
2023-02-21 20:32:50 -0800

    Add isPositive.py problem statement.

---
+++ simpleSequential/isPositive.py
@@ -0,0 +1,5 @@
+# Create a function that take one integer number and return True if it is positive, otherwise return False.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   isPositive(5) return True
+#   isPositive(0) return False
