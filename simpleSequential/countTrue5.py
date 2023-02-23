84f00d79ffb3e69d88aab99b50c142d3ba8e0263
Khoa Le <rubikvn2100@gmail.com>
2023-02-22 23:10:41 -0800

    Add countTrue5.py problem statement.

---
+++ simpleSequential/countTrue5.py
@@ -0,0 +1,13 @@
+# Note: True and False are just a human readable representation of boolean type. Behind the screen, it is interger.
+#       If you use boolean in an expression, you will see that in Python, True is 1 and False is 0.
+# Ex:
+#   print(True)                # True
+#   print(True + False + True) # 2
+#   print((True + True) * 420) # 840
+#   print((False + True) * 69) # 69
+#
+# Create a function that take five boolean values. Return the number of True arguments.
+# This function is simple and you will not need to use condition structure.
+# Ex: 
+#   countTrue5(True, False, True, False, False) return 2
+#   countTrue5(True, True, True, False, False)  return 3
