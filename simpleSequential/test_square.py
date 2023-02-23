b9277e386db89c7323e0c811728fbe5cc2b53ff6
Khoa Le <rubikvn2100@gmail.com>
2023-02-22 22:35:39 -0800

    Change the test function name of test_square.py as part of refactor.

--- simpleSequential/test_square.py
+++ simpleSequential/test_square.py
@@ -2,13 +2,13 @@ import unittest
 from square import square
 
 class TestSquareFunction(unittest.TestCase):
-    def test_squareFunctionCase1(self):
+    def test_positiveInput(self):
         self.assertEqual(square(5), 25)
 
-    def test_squareFunctionCase2(self):
+    def test_negativeInput(self):
         self.assertEqual(square(-3), 9)
 
-    def test_squareFunctionCase3(self):
+    def test_specialZeroInput(self):
         self.assertEqual(square(0), 0)
 
 if __name__ == "__main__":
