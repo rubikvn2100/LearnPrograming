3b25e439072080b0877c550d9c224e2f402cc74f
Khoa Le <rubikvn2100@gmail.com>
2023-02-22 22:54:40 -0800

    Add isDivisibleBy42.py problem statement.

---
+++ simpleSequential/isDivisibleBy42.py
@@ -0,0 +1,5 @@
+# Create a function that take one integer number and return True if it is divisible by 42, otherwise return False.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   isDivisibleBy42(69) return True
+#   isDivisibleBy42(42) return False
