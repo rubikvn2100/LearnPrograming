5a1182830ebe10625e2169e041e60f93f706cf14
Dat Nguyen <tiendatlg1234@gmail.com>
2023-02-27 13:43:22 +0700

    Rename and add solution for isTwoIntegerEqual.py problem.

--- simpleSequential/isTwoIntegerEqual.py
+++ simpleSequential/isTwoIntegerEqual.py
@@ -1,2 +1,4 @@
 # Create a function that take two integer numbers and return True if they are equal, otherwise return False.
 # This function is simple and you will not need to use condition structure.
+def isTwoIntegerEqual(n1: int, n2: int) -> bool:
+    return n1 == n2
