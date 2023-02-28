ddb8f71da7cb01352f0bc817492479805f626760
Khoa Le <rubikvn2100@gmail.com>
2023-02-28 00:37:57 -0800

    Add isStringEmpty.py problem statement.

---
+++ simpleSequential/isStringEmpty.py
@@ -0,0 +1,8 @@
+# Note: Comparison operation can be use with string just as interger and boolean.
+#       Comparison operation are `<`, `>`, `<=`, `>=`, `==`, `!=`
+#
+# Create a function that take one string, and return True if the string is empty, otherwise return False.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   isStringEmpty("")            # return True
+#   isStringEmpty("Hello World") # return False
