9322d3ae02d782217aaf0f28618c2857c8d92ef6
Khoa Le <rubikvn2100@gmail.com>
2023-02-28 01:24:11 -0800

    Add integerNegate.py problem statement.

---
+++ simpleSequential/integerNegate.py
@@ -0,0 +1,5 @@
+# Create a function that take one integer number and negate the value.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   integerNegate(-5) return 5
+#   integerNegate(3)  return -3
