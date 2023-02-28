3c589d5d029345b3d701dc55afd5d67f1f11d4ac
Khoa Le <rubikvn2100@gmail.com>
2023-02-28 01:26:25 -0800

    Add booleanNegate.py problem statement.

---
+++ simpleSequential/booleanNegate.py
@@ -0,0 +1,5 @@
+# Create a function that take one boolean variable and negate the value.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   booleanNegate(True)  return False
+#   booleanNegate(False) return True
