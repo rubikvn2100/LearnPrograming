17dd24dd79d71825bc406b8a77034035a467f013
Khoa Le <rubikvn2100@gmail.com>
2023-02-28 00:36:34 -0800

    Add isTwoStringEqual.py problem statement.

---
+++ simpleSequential/isTwoStringEqual.py
@@ -0,0 +1,10 @@
+# Note: Comparison operation can be use with string just as interger and boolean.
+#       Comparison operation are `<`, `>`, `<=`, `>=`, `==`, `!=`
+#
+# Create a function that take two strings as input. Return True if they are equal, otherwise return False.
+# This function is simple and you will not need to use condition structure.
+# Ex:
+#   isTwoStringEqual("abc", "xyz")     # return False
+#   isTwoStringEqual("Hello", "hello") # return False
+#   isTwoStringEqual("Hello", "Hello") # return True
+#   isTwoStringEqual("", "")           # return true
