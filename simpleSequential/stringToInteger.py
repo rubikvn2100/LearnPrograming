6c4637aa5b827e1b06a0c15175bbf94d9de03298
Khoa Le <rubikvn2100@gmail.com>
2023-03-01 23:24:46 -0800

    Add "stringToInteger.py" problem statement.

---
+++ simpleSequential/stringToInteger.py
@@ -0,0 +1,8 @@
+# Note: Sometime we need to convert string to integer. It can be done using build in function.
+#
+# Create a function that take one string. Return the number representation of the string.
+# Assume that the input string can be represent as a valid number.
+# Ex:
+#   stringToInteger("690") # return 690
+#   stringToInteger("-69") # return -69
+#   stringToInteger("000") # return 0
