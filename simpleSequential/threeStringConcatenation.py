cd99c6571ae0b9b4f6d7d5d2823b6f29e875cdc1
Khoa Le <rubikvn2100@gmail.com>
2023-03-01 21:20:28 -0800

    Add "threeStringConcatenation.py" problem statement.

---
+++ simpleSequential/threeStringConcatenation.py
@@ -0,0 +1,9 @@
+# Note: strings can be concatenated using `+` operator
+# Ex:
+#   print("Hello " + "world") # Displayed "Hello world"
+#   print("good" + " day")    # Displayed "good day"
+#
+# Create a function that take three strings and return the concatenation of them.
+# Ex:
+#   threeStringConcatenation("Hello ", "world", '!')   # return "Hello world"
+#   threeStringConcatenation("good", '-', "day")       # return "good-day"
