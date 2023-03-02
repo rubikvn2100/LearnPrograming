f72a34fa6f57fddfd09d5d554d1bb5361a6ee6c0
Khoa Le <rubikvn2100@gmail.com>
2023-03-01 23:27:18 -0800

    Add "addTwoNumericalString.py" problem statement.

---
+++ simpleSequential/addTwoNumericalString.py
@@ -0,0 +1,10 @@
+# Note: Sometime we want to add two strings that represent number together. 
+#       It can be done using multiple build in functions.
+#
+# Create a function that take two strings that represent two numbers.
+# Return a string that represent the sum of those two numbers represent by the input strings.
+# Assume that the input strings can be represent as valid numbers.
+# Ex:
+#   addTwoNumericalString("321", "-21") # return "300"
+#   addTwoNumericalString("042", "-42") # return "0"
+
