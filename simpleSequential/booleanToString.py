43992a1b5297b7eac96d3a79f4102aad3d301be3
Khoa Le <rubikvn2100@gmail.com>
2023-03-01 23:26:23 -0800

    Add "booleanToString.py" problem statement.

---
+++ simpleSequential/booleanToString.py
@@ -0,0 +1,6 @@
+# Note: Sometime we need to convert boolean to string. It can be done using a build in function.
+#
+# Create a function that take one boolean variable and return the string representation of the boolean.
+# Ex:
+#   booleanToString(True)  # return "True"
+#   booleanToString(False) # return "False"
