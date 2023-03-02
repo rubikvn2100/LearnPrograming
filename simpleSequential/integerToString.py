a9e5b1b8352bdd8675263c969d9eba19eb542abe
Khoa Le <rubikvn2100@gmail.com>
2023-03-01 23:25:44 -0800

    Add "integerToString.py" problem statement.

---
+++ simpleSequential/integerToString.py
@@ -0,0 +1,7 @@
+# Note: Sometime we need to convert integer to string. It can be done using a build in function.
+#
+# Create a function that take one integer number. Return the string representation of the number.
+# Ex:
+#   integerToString(690) # return "690"
+#   integerToString(-69) # return "-69"
+#   integerToString(000) # return "0"
