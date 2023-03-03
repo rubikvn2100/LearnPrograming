9d536329b335e36549f76e2a25eb04bca36b297f
Khoa Le <rubikvn2100@gmail.com>
2023-03-02 23:48:53 -0800

    Add "makeCoordinateTuple.py" problem statement.

---
+++ simpleSequential/makeCoordinateTuple.py
@@ -0,0 +1,16 @@
+# Note: In Python, a tuple is an ordered, immutable collection of elements enclosed in parentheses `()`.
+#       A tuple can contain any number of elements, which can be of different data types,
+#       and the elements are separated by commas.
+#         Ex:
+#           print((2))       # Displayed (2)
+#           print((2, 3))    # Displayed (2, 3)
+#           print((2, 3, 5)) # Displayed (2, 4, 5)
+#
+# We want to represent coordinate with a pair of variable `x`, `y`.
+# But, we don't want to carry them seperately because they lose their meaning when sepearte.
+#
+# Create a function that take two integer variables `x` and `y`.
+# Return a tuple `(x, y)` which represent the coordinate.
+# Ex:
+#   makeCoordinateTuple(10, 1) # return (10, 1)
+#   makeCoordinateTuple(-5, 3) # return (-5, 3)
