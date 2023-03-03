0da1da4dcdcee36ab774fbf3259bd14da9728837
Khoa Le <rubikvn2100@gmail.com>
2023-03-02 23:38:17 -0800

    Add "floorDivision.py" problem statement.

---
+++ simpleSequential/floorDivision.py
@@ -0,0 +1,11 @@
+# Note: When making division, you will receive a float not an integer.
+#         Ex:
+#           print(4 / 2) # Displayed 2.0 not 2
+#           pintt(9 / 2) # Displayed 4.5 not 4
+#       But you might only need the integer part. It can be done using a build in floor division operator.
+#
+# Create a function that take two integer variables `dividend` and `divisor`.
+# Return an integer which is the result of the floor division when `dividend` is divided by `divisor`.
+# Ex:
+#   floorDivision(18, 3) # return 6
+#   floorDivision(19, 4) # return 4
