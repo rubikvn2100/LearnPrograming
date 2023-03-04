5fe9ed9eaa178b7a8ff1141737732aadd2ca2fd9
Khoa Le <rubikvn2100@gmail.com>
2023-03-04 01:58:36 -0800

    Add "floatToInteger.py" problem statement.

---
+++ simpleSequential/floatToInteger.py
@@ -0,0 +1,9 @@
+# Note: Sometime we need to convert float to integer. It can be done using a build in function.
+#       This can be use as a way to round down a float number.
+#
+# Create a function that take one float number.
+# Return an integer which is the integer part of the input.
+# Ex:
+#   floatToInteger(0.012) # return 0
+#   floatToInteger(1.234) # return 1
+#   floatToInteger(123.4) # return 123
