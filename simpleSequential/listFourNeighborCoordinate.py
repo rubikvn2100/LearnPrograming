4dd5c4ccc3aded2e314d795e88d16dfd00394454
Dat Nguyen <tiendatlg1234@gmail.com>
2023-03-20 22:58:07 -0700

    Add solution for "listFourNeighborCoordinate.py" problem.

--- simpleSequential/listFourNeighborCoordinate.py
+++ simpleSequential/listFourNeighborCoordinate.py
@@ -12,4 +12,8 @@
 # Ex:
 #   listFourNeighborCoordinate((5, 6))   # return [(5, 7), (5, 5), (6, 6), (4, 6)]
 #   listFourNeighborCoordinate((-2, -3)) # return [(-2, -2), (-2, -4], (-1, -3), (-3, -3)]
+from typing import List, Tuple
 
+def listFourNeighborCoordinate(coordinate:Tuple[int, int]) -> List[Tuple[int, int]]:
+    x, y = coordinate
+    return [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
