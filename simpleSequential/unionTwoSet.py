962248d3d4ee4725df110dddab6a3a701aa79294
Khoa Le <rubikvn2100@gmail.com>
2023-03-20 23:47:14 -0700

    Add "unionTwoSet.py" problem statement.

---
+++ simpleSequential/unionTwoSet.py
@@ -0,0 +1,5 @@
+# Create a function that take two set of integers and return the union of them
+# Ex:
+#   unionTwoSet({2, 3}, {3, 2}) # Return a set that equal to {2, 3}
+#   unionTwoSet({2, 3}, {4, 3}) # Return a set that equal to {2, 3, 4}
+#   unionTwoSet({2, 3}, {5, 4}) # Return a set that equal to {2, 3, 4, 5}
