3d30d15eb00eff38bfaa94098e859e90337e3eda
Khoa Le <rubikvn2100@gmail.com>
2023-03-20 23:41:46 -0700

    Add "isTwoSetEqual.py" problem statement.

---
+++ simpleSequential/isTwoSetEqual.py
@@ -0,0 +1,13 @@
+# Note: In Python, a set is an unordered collection of unique elements.
+#       It is defined using curly braces {} or the set() constructor.
+#         Ex:
+#           print({2, 3})      # Displayed {2, 3} or {3, 2}
+#           print(set([2, 3])) # Displayed {2, 3} or {3, 2}
+#
+# Create a function that take two set of integers.
+# Return True if they are equal set, otherwise return False.
+# Ex:
+#   isTwoSetEqual({2, 3}, {3, 2})          # Return True
+#   isTwoSetEqual({2, 3}, {2, 3, 2, 2, 3}) # Return True
+#   isTwoSetEqual({2, 3}, {2, 3, 1, 5, 4}) # Return False
+#   isTwoSetEqual({2, 3}, {123, 456, 789}) # Return False
