266edec3bb4f0a5d21de14e1e343f60ddc0f893f
Khoa Le <rubikvn2100@gmail.com>
2023-03-20 23:48:51 -0700

    Add "isEmptySet.py" problem statement.

---
+++ simpleSequential/isEmptySet.py
@@ -0,0 +1,5 @@
+# Create a function that take one set of integers as input.
+# Return True if the set is empty, otherwise return False.
+# Ex:
+#   isEmptySet({})     # Return True
+#   isEmptySet({2, 3}) # Return False
