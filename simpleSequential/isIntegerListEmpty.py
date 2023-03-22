d6714dbc51a0292b215e539adb1dc8c7a792ec19
Khoa Le <rubikvn2100@gmail.com>
2023-03-22 02:03:34 -0700

    Add "isIntegerListEmpty.py" problem statement.

---
+++ simpleSequential/isIntegerListEmpty.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as an input.
+# Return True if the list is empty, and False otherwise.
+# Ex:
+#   isIntegerListEmpty([])        # return True
+#   isIntegerListEmpty([0, 1, 2]) # return False
