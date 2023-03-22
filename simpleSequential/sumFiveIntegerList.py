63fb1789b2fd33fc872851761b6bb615d5a18488
Khoa Le <rubikvn2100@gmail.com>
2023-03-22 02:04:48 -0700

    Add "sumFiveIntegerList.py" problem statement.

---
+++ simpleSequential/sumFiveIntegerList.py
@@ -0,0 +1,8 @@
+# Note: For the purpose of improving your programming skills,
+#       please refrain from using the built-in sum() function.
+#
+# Create a function that takes a list of five integers as an input.
+# Return the sum of the numbers in the list.
+# Ex:
+#   sumFiveIntegerList([-5, -3, 2, 6, 0]) # return 0
+#   sumFiveIntegerList([1, 5, 2, 4, 3])   # return 15
