0188e6194965ead8684d2de1ceccc8a0e366f58b
Khoa Le <rubikvn2100@gmail.com>
2023-03-22 02:02:35 -0700

    Add "getIntegerListLength.py" problem statement.

---
+++ simpleSequential/getIntegerListLength.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as an input and returns the length of the list.
+# Ex:
+#   getIntegerListLength([])              # return 0
+#   getIntegerListLength([0, 1, 2])       # return 3
+#   getIntegerListLength([4, 3, 2, 1, 0]) # return 5
