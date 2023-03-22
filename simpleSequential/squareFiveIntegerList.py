3efee90d5ecdd0fbcf03f9461f78ef97de0c1135
Khoa Le <rubikvn2100@gmail.com>
2023-03-22 02:05:47 -0700

    Add "squareFiveIntegerList.py" problem statement.

---
+++ simpleSequential/squareFiveIntegerList.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of five integers as an input.
+# Return a new list containing the square of each integer in the original list.
+# Ex:
+#   squareFiveIntegerList([-2, -1, 0, 1, 2]) # return [4, 1, 0, 1, 4]
+#   squareFiveIntegerList([3, 5, 9, -2, -7]) # return [9, 25, 81, 4, 49]
