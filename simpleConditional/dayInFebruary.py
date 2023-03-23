900ee200111d3f29c1c3ff7a4eba34fd0b581b4b
Khoa Le <rubikvn2100@gmail.com>
2023-03-22 23:13:56 -0700

    Add "dayInFebruary.py" problem statement.

---
+++ simpleConditional/dayInFebruary.py
@@ -0,0 +1,12 @@
+# Note: Conditional structure allow a program to execute one of two available code blocks based on some boolean expression.
+#       When using them in a function, a simple conditional structure is:
+#       def foo(n):
+#           if <boolean expression base on n>:
+#               return <expression 1>
+#
+#           return <expression 2>
+#
+# Create a function that takes an integer representing a year as input and returns the number of days in February.
+# Ex:
+#   dayInFebruary(2023) # return 28
+#   dayInFebruary(2024) # return 29
