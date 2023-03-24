2f57f0c0da210c9dd215476ef158ffb48e2752bc
Khoa Le <rubikvn2100@gmail.com>
2023-03-23 23:14:19 -0700

    Add "countPositiveAndNegativeInteger.py" problem statement.

---
+++ simpleLooping/countPositiveAndNegativeInteger.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as input.
+# Return a tuple of two integers representing the count of positive and negative integers in the list, in that order.
+# Ex:
+#   countPositiveAndNegativeInteger([-9, -5, -2, -6, -3, -1]) # return (0, 6)
+#   countPositiveAndNegativeInteger([3, 5, 7, -2, 6, -1, -3]) # return (4, 3)
