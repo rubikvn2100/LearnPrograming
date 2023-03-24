e0101259fb8935abe4ad00f13628580ee139e44e
Khoa Le <rubikvn2100@gmail.com>
2023-03-23 23:15:45 -0700

    Add "getLargest.py" problem statement.

---
+++ simpleLooping/getLargest.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as input.
+# Return the largest number in the list.
+# Ex:
+#   getLargest([-9, -5, -2, -6, -3, -1]) # return -1
+#   getLargest([3, 5, 7, -2, 6, -1, -3]) # return 7
