43034dcfaafe98dc469124e14f84c7cd70f2b550
Dat Nguyen <tiendatlg1234@gmail.com>
2023-03-23 22:50:36 -0700

    Add solution for "reverseFiveIntegerList.py" problem.

--- simpleSequential/reverseFiveIntegerList.py
+++ simpleSequential/reverseFiveIntegerList.py
@@ -3,3 +3,7 @@
 # Ex:
 #   reverseFiveIntegerList([-2, -1, 0, 1, 2]) # return [2, 1, 0, -1, -2]
 #   reverseFiveIntegerList([3, 5, 9, -2, -7]) # return [-7, -2, 9, 5, 3]
+from typing import List
+
+def reverseFiveIntegerList(lst: List[int]) -> List[int]:
+    return [lst[4], lst[3], lst[2], lst[1], lst[0]]
