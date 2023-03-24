4d79bafe17b6fb38a5b80406d9271c43342dadcc
Khoa Le <rubikvn2100@gmail.com>
2023-03-23 23:12:10 -0700

    Add "sumIntegerSquareToN.py" problem statement.

---
+++ simpleLooping/sumIntegerSquareToN.py
@@ -0,0 +1,7 @@
+# Create a function that takes an integer N.
+# Return the sum of the squares of all positive integers that are less than N.
+# If the input is less than 0, return -1.
+# Ex:
+#   sumIntegerSquareToN(-100000) # return -1
+#   sumIntegerSquareToN(3)       # return 5
+#   sumIntegerSquareToN(1234567) # return 627223338685887771
