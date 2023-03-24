617597a163419f61374c23abd1e2135ada12c02b
Khoa Le <rubikvn2100@gmail.com>
2023-03-23 23:13:38 -0700

    Add "countPositiveInteger.py" problem statement.

---
+++ simpleLooping/countPositiveInteger.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as input.
+# Return the number of positive integers in the list.
+# Ex:
+#   countPositiveInteger([-9, -5, -2, -6, -3, -1]) # return 0
+#   countPositiveInteger([3, 5, 7, -2, 6, -1, -3]) # return 4
