3f37e6fad12e455b75c4849627756b657e8d74fa
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 00:41:09 -0700

    Add "maxTwoNumber.py" problem statement.

---
+++ simpleConditional/maxTwoNumber.py
@@ -0,0 +1,15 @@
+# Note: In Python, there is a syntax shortcut that allows us to write a short conditional structure.
+#       This is known as the ternary operator in other programming languages.
+#       Here's an example of the ternary operator in Python:
+#
+#           def integerAbsoluteValue(n: int) -> int:
+#               return -n if n < 0 else n
+#
+#           def dayInFebruary(year: int) -> int:
+#               return 29 if year % 4 else 28
+#
+# Create a that takes two integers.
+# Return the larger number.
+# Ex:
+#   maxTwoNumber(12, 3) # return 12
+#   maxTwoNumber(-2, 3) # return 3
