29a5c83a90bc43919da4e1d16e1d6bf5969d7b0a
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 00:41:52 -0700

    Add "minMaxTwoNumber.py" problem statement.

---
+++ simpleConditional/minMaxTwoNumber.py
@@ -0,0 +1,5 @@
+# Create a that takes two integers.
+# Return a tuple of two integers representing the smallest and largest values in that order.
+# Ex:
+#   minTwoNumber(12, 3) # return (3, 12)
+#   minTwoNumber(-2, 3) # return (-2, 3)
