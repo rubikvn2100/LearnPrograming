8ce5764d82dcf4b4807a1e7371268dbb1cc7728c
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 00:43:11 -0700

    Add "sortThreeNumberAscending.py" problem statement.

---
+++ simpleConditional/sortThreeNumberAscending.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of three integers as input.
+# Return a new list of the same three integers, sorted in ascending order.
+# Ex:
+#   sortThreeNumberAscending([1, 3, 2]) # return [1, 2, 3]
+#   sortThreeNumberAscending([2, 1, 3]) # return [1, 2, 3]
