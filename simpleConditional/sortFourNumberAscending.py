0b19344d56e66c2a2fd5094967fef5189841ad33
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 00:44:56 -0700

    Add "sortFourNumberAscending.py" problem statement.

---
+++ simpleConditional/sortFourNumberAscending.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of four integers as input.
+# Return a new list of the same four integers, sorted in ascending order.
+# Ex:
+#   sortFourNumberAscending([2, 4, 1, 3]) # return [1, 2, 3, 4]
+#   sortFourNumberAscending([3, 1, 4, 2]) # return [1, 2, 3, 4]
