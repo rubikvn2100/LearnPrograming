079a0c63a77ff65aedd8d336e8cc811549eb87d9
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 00:40:26 -0700

    Add "minTwoNumber.py" problem statement.

---
+++ simpleConditional/minTwoNumber.py
@@ -0,0 +1,15 @@
+# Note: In Python, there is a syntax shortcut that allows us to write a short conditional structure.
+#       This is known as the ternary operator in other programming languages.
+#       Here's an example of the ternary operator in Python:
+#
+#           def integerAbsoluteValue(n: int) -> int:
+#               return -n if n < 0 else n
+#
+#           def dayInFebruary(year: int) -> int:
+#               return 29 if year % 4 == 0 else 28
+#
+# Create a that takes two integers.
+# Return the smaller number.
+# Ex:
+#   minTwoNumber(12, 3) # return 3
+#   minTwoNumber(-2, 3) # return -2
