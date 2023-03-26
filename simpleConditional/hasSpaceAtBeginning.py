6cb029ae0ae90455814396cf68963b02ab5c22b1
Khoa Le <rubikvn2100@gmail.com>
2023-03-25 23:22:13 -0700

    Add "hasSpaceAtBeginning.py" problem statement.

---
+++ simpleConditional/hasSpaceAtBeginning.py
@@ -0,0 +1,6 @@
+# Create a function that takes a string as input.
+# Return True if the input string has one or more spaces at the beginning, and False otherwise.
+# Ex:
+#   hasSpaceAtBeginning("")                  # return False
+#   hasSpaceAtBeginning("Hello  World!")     # return False
+#   hasSpaceAtBeginning("  Hello  World!  ") # return True
