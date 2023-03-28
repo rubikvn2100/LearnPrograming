9e3aeb5d485c53790f2e8a45dc14beefea169237
Khoa Le <rubikvn2100@gmail.com>
2023-03-27 22:18:14 -0700

    Add "isBinaryList.py" problem statement.

---
+++ simpleLooping/isBinaryList.py
@@ -0,0 +1,6 @@
+# Create a function that takes a list of integers as input.
+# The function should return True if the list represents a binary value where it only contains values 0 or 1.
+# Return False otherwise.
+# Ex:
+#   isBinaryList([0, 1, 2, 3, 4]) # return False
+#   isBinaryList([1, 0, 0, 1, 1]) # return True
