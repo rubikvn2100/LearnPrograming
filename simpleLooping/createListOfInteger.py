2005549b1b007a2603c61fbdc1b5a8f79d75b497
Khoa Le <rubikvn2100@gmail.com>
2023-03-27 22:08:14 -0700

    Add "createListOfInteger.py" problem statement.

---
+++ simpleLooping/createListOfInteger.py
@@ -0,0 +1,12 @@
+# Note: In Python, we can add an element to the end of a list using the append method:
+#         Ex:
+#           my_list = [0, 1, 2]
+#           my_list.append(3)
+#           print(my_list) # displays [0, 1, 2, 3]
+#
+# Create a function that takes an integer N as input.
+# The function should return a list of integers from 0 to N - 1 in ascending order.
+# If N is non-positive, return an empty list.
+# Ex:
+#   createListOfInteger(0) # return []
+#   createListOfInteger(4) # return [0, 1, 2, 3]
