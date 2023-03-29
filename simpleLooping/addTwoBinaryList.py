5df02654fe83be388b7b724e353cc6f8f1aa5cac
Khoa Le <rubikvn2100@gmail.com>
2023-03-28 22:05:21 -0700

    Add "addTwoBinaryList.py" problem statement.

---
+++ simpleLooping/addTwoBinaryList.py
@@ -0,0 +1,8 @@
+# Create a function that takes two binary lists as input.
+# The function should return the sum of the two binary numbers as a binary list.
+# The result list should have the same length as the longer input list.
+# If we need one more element to store the extra carry value, then append it at the end of the list.
+# If the input does not represent two binary lists, return -1.
+# Ex:
+#   addTwoBinaryList([1, 0, 1], [1, 0, 0, 1]) # return [0, 1, 1, 1]
+#   addTwoBinaryList([1, 1, 0], [1, 0, 1, 1]) # return [0, 0, 0, 0, 1]
