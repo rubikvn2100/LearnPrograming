5343f08d5694c708884b752a8d2c13506595131a
Khoa Le <rubikvn2100@gmail.com>
2023-03-28 22:07:21 -0700

    Add "createBinaryStringList.py" problem statement.

---
+++ simpleLooping/createBinaryStringList.py
@@ -0,0 +1,9 @@
+# Create a function that takes an integer N as input.
+# The function should return a list of binary strings.
+# Where each string represents a binary value from 0 to N - 1 in ascending order.
+# Ex:
+#   createBinaryStringList(0) # return ["0"]
+#   createBinaryStringList(2) # return ["0", "1"]
+#   createBinaryStringList(4) # return ["0", "1", "10", "11"]
+#   createBinaryStringList(6) # return ["0", "1", "10", "11", "100", "101"]
+#   createBinaryStringList(8) # return ["0", "1", "10", "11", "100", "101", "110", "111"]
