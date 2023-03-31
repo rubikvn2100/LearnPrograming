64c413b3a978a37786748bd975466b985ddd4223
Khoa Le <rubikvn2100@gmail.com>
2023-03-30 21:55:25 -0700

    Add "removeSpaceAtEnd.py" problem statement.

---
+++ simpleLooping/removeSpaceAtEnd.py
@@ -0,0 +1,19 @@
+# Note: In Python, we can use slicing to get a sublist of a list or string.
+#       For example, to get a sublist of a list without the last two elements:
+#
+#         lst = [2, 3, 5, 7, 11]
+#         subList = lst[:-2] # Note: lst[:3] aslo work, but we need to know the cut off index is 3.
+#         print(subList) # Display [2, 3, 5]
+#
+#       Similarly, to get a sublist of a string without the last two characters:
+#
+#         str = "Python"
+#         subStr = str[:-2] # Note: lst[:4] aslo work, but we need to know the cut off index is 4.
+#         print(subStr) # Display "Pyth"
+#
+# Create a function that takes a string as input.
+# Remove all spaces in the end of the list.
+# Ex:
+#   removeSpaceAtEnd("")                  # return ""
+#   removeSpaceAtEnd("Hello  World!")     # return "Hello  World!"
+#   removeSpaceAtEnd("  Hello  World!  ") # return "  Hello  World!"
