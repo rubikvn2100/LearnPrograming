efd3b57aa18a9f80588fb40137b59f2ba6963c38
Khoa Le <rubikvn2100@gmail.com>
2023-04-01 12:58:09 -0700

    Add "twoNumberGreatestCommonDivisor.py" problem statement.

---
+++ simpleLooping/twoNumberGreatestCommonDivisor.py
@@ -0,0 +1,8 @@
+# Create a function that takes two integers as input.
+# Return the Greatest Common Divisor of them using Euclidean algorithm.
+# Return -1 if both input are zeros as GCD(0, 0) is undefined.
+# Ex:
+#   twoNumberGreatestCommonDivisor(163, 0) # return 163
+#   twoNumberGreatestCommonDivisor(24, 36) # return 12
+#   twoNumberGreatestCommonDivisor(-6, 12) # return 6
+#   twoNumberGreatestCommonDivisor(-6, -3) # return 3
