fed3917eb886ad6b37a9ff357ac77a69740e9e55
Dat Nguyen <tiendatlg1234@gmail.com>
2023-04-02 14:21:24 -0700

    Add solution for "getSmallest.py" problem.

--- simpleLooping/getSmallest.py
+++ simpleLooping/getSmallest.py
@@ -3,3 +3,17 @@
 # Ex:
 #   getSmallest([-9, -5, -2, -6, -3, -1]) # return -9
 #   getSmallest([3, 5, 7, -2, 6, -1, -3]) # return -3
+from typing import List
+
+def getSmallest(lst: List[int]) -> int:
+    lenLst = len(lst)
+
+    i = 1
+    smallestNumber = lst[0]
+    while i < lenLst:
+        if lst[i] < smallestNumber:
+            smallestNumber = lst[i]
+
+        i = i + 1
+
+    return smallestNumber
