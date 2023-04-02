251106a1f5067036988eed8d67d88b17709c111b
Dat Nguyen <tiendatlg1234@gmail.com>
2023-04-02 14:23:09 -0700

    Add solution for "getLargest.py" problem.

--- simpleLooping/getLargest.py
+++ simpleLooping/getLargest.py
@@ -3,3 +3,17 @@
 # Ex:
 #   getLargest([-9, -5, -2, -6, -3, -1]) # return -1
 #   getLargest([3, 5, 7, -2, 6, -1, -3]) # return 7
+from typing import List
+
+def getLargest(lst: List[int]) -> int:
+    lenLst = len(lst)
+
+    i = 1
+    largestNumber = lst[0]
+    while i < lenLst:
+        if lst[i] > largestNumber:
+            largestNumber = lst[i]
+
+        i = i + 1
+
+    return largestNumber
