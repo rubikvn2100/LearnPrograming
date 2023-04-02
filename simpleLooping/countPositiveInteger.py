b023a944126fd8b3d06ca98019c107c7c4186131
Dat Nguyen <tiendatlg1234@gmail.com>
2023-04-02 15:57:13 -0700

    Add solution for "countPositiveInteger.py" problem.

--- simpleLooping/countPositiveInteger.py
+++ simpleLooping/countPositiveInteger.py
@@ -3,3 +3,17 @@
 # Ex:
 #   countPositiveInteger([-9, -5, -2, -6, -3, -1]) # return 0
 #   countPositiveInteger([3, 5, 7, -2, 6, -1, -3]) # return 4
+from typing import List
+
+def countPositiveInteger(lst) -> int:
+    lenLst = len(lst)
+    
+    i = 0
+    count = 0
+    while i < lenLst:
+        if lst[i] > 0:
+            count = count + 1
+       
+        i = i + 1
+    
+    return count
