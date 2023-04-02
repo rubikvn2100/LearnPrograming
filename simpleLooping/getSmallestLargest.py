0fcd41bcb535f73beb1d993ce93e100dd84dd2cb
Dat Nguyen <tiendatlg1234@gmail.com>
2023-04-02 15:17:56 -0700

    Add solution for "getSmallestLargest.py" problem.

--- simpleLooping/getSmallestLargest.py
+++ simpleLooping/getSmallestLargest.py
@@ -3,3 +3,21 @@
 # Ex:
 #   countPositiveAndNegativeInteger([-9, -5, -2, -6, -3, -1]) # return (-9, -1)
 #   countPositiveAndNegativeInteger([3, 5, 7, -2, 6, -1, -3]) # return (-3, 7)
+from typing import List, Tuple
+
+def getSmallestLargest(lst: List[int]) -> Tuple[int, int]:
+    lenLst = len(lst)
+    
+    smallestNumber = lst[0]
+    largestNumber = lst[0]
+    i = 1
+    while i < lenLst:
+        if lst[i] < smallestNumber:
+            smallestNumber = lst[i]
+
+        if lst[i] > largestNumber:
+            largestNumber = lst[i]
+        
+        i = i + 1
+
+    return (smallestNumber, largestNumber)
