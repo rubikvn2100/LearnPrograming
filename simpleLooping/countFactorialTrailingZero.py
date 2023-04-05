172d4761ad10ebb6944d16d7819bd516422b13a1
Khoa Le <rubikvn2100@gmail.com>
2023-04-04 23:03:24 -0700

    Add "countFactorialTrailingZero.py" problem statement.

---
+++ simpleLooping/countFactorialTrailingZero.py
@@ -0,0 +1,9 @@
+# Create a function that takes an integer N as input.
+# Return the number of trailing zero of N factorial.
+# Ex:
+#   countFactorialTrailingZero(7)             # return 1
+#   countFactorialTrailingZero(12)            # return 2
+#   countFactorialTrailingZero(183)           # return 44
+#   countFactorialTrailingZero(343118)        # return 85773
+#   countFactorialTrailingZero(722935669)     # return 180733910
+#   countFactorialTrailingZero(525242356355)  # return 131310589082
