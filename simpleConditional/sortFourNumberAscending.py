8acc1847cce4747fdca2217d38070259081105f9
Dat Nguyen <tiendatlg1234@gmail.com>
2023-04-04 23:52:30 -0700

    Add solution for "sortFourNumberAscending.py" problem.

--- simpleConditional/sortFourNumberAscending.py
+++ simpleConditional/sortFourNumberAscending.py
@@ -3,3 +3,22 @@
 # Ex:
 #   sortFourNumberAscending([2, 4, 1, 3]) # return [1, 2, 3, 4]
 #   sortFourNumberAscending([3, 1, 4, 2]) # return [1, 2, 3, 4]
+from typing import List
+
+def sortFourNumberAscending(lst: List[int]) -> List[int]:
+    if lst[0] > lst[1]:
+        lst[0], lst[1] = lst[1], lst[0]
+
+    if lst[2] > lst[3]:
+        lst[2], lst[3] = lst[3], lst[2]
+
+    if lst[0] > lst[2]:
+        lst[0], lst[2] = lst[2], lst[0]
+
+    if lst[1] > lst[3]:
+        lst[1], lst[3] = lst[3], lst[1]
+
+    if lst[1] > lst[2]:
+        lst[1], lst[2] = lst[2], lst[1]
+
+    return lst
