d4adfd1d543e3b4dfebe2e06e3a10981185a3c12
Khoa Le <rubikvn2100@gmail.com>
2023-04-04 22:58:49 -0700

    Add "isPrime.py" problem statement.

---
+++ simpleLooping/isPrime.py
@@ -0,0 +1,5 @@
+# Create a function that takes one integer as input.
+# Return True if the input is a prime number, otherwise return False.
+# Ex:
+#   isPrime(937453153313) # return True
+#   isPrime(965246846246) # return False
