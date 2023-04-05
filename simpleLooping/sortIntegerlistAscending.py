39a9109c2450ce8095493c1fb6a799f63126d0d6
Khoa Le <rubikvn2100@gmail.com>
2023-04-04 23:01:28 -0700

    Add "sortIntegerlistAscending.py" problem statement.

---
+++ simpleLooping/sortIntegerlistAscending.py
@@ -0,0 +1,5 @@
+# Create a function that takes a list of integers as input.
+# Return a new list of the same integers, sorted in ascending order.
+# Ex:
+#   sortIntegerlistAscending([482, -5, 15, -86, 68, 381]) # return [-86, -5, 15, 68, 381, 482]
+#   sortIntegerlistAscending([5, 1, 6, 9, 7, 4, 3, 2, 8]) # return [1, 2, 3, 4, 5, 6, 7, 8, 9]
