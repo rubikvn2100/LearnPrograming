62c0980f1e9747095cde2a23a11364d0c25797dc
Khoa Le <rubikvn2100@gmail.com>
2023-04-04 22:57:59 -0700

    Add "listGreatestCommonDivisor.py" problem statement.

---
+++ simpleLooping/listGreatestCommonDivisor.py
@@ -0,0 +1,6 @@
+# Create a function that takes a list of integers as input.
+# Return the Greatest Common Divisor of them using Euclidean algorithm.
+# Return -1 if at least two integers are zeros as GCD(0, 0) is undefined.
+# Ex:
+#   listGreatestCommonDivisor(-42, 402, -66, 354, 78, 426) # return 6
+#   listGreatestCommonDivisor(7883933, 91928167, 54151651) # return 1
