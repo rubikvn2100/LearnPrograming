5f1c5cf3bc0c2f5fa6679cff626afd5ca6394072
Dat Nguyen <tiendatlg1234@gmail.com>
2023-05-01 15:04:15 -0700

    Add "twoNumberGreatestCommonDivisor.py" problem statement.

--- simpleLooping/twoNumberGreatestCommonDivisor.py
+++ simpleLooping/twoNumberGreatestCommonDivisor.py
@@ -6,3 +6,22 @@
 #   twoNumberGreatestCommonDivisor(24, 36) # return 12
 #   twoNumberGreatestCommonDivisor(-6, 12) # return 6
 #   twoNumberGreatestCommonDivisor(-6, -3) # return 3
+def twoNumberGreatestCommonDivisor(num1: int, num2: int) -> int:
+    if num1 == 0 and num2 == 0:
+        return -1
+
+    if num1 < 0:
+        num1 = -num1
+
+    if num2 < 0:
+        num2 = -num2
+
+    if num1 < num2:
+       num1, num2 = num2, num1
+
+    r = num1 % num2
+    while r != 0:
+        num1, num2 = num2, r
+        r = num1 % num2
+
+    return num2
