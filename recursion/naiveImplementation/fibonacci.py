081f3506a02ba9e6ad88e355a2e9f042a3bf3e1f
Khoa Le <rubikvn2100@gmail.com>
2023-05-02 00:12:18 -0700

    Add "fibonacci.py" problem statement.

---
+++ recursion/naiveImplementation/fibonacci.py
@@ -0,0 +1,14 @@
+# Note: Recursion is a programming technique where a function calls itself to
+#       solve a problem or perform a task. Recursion use to have the following structure:
+#
+#           def recursionFunction(<parameters>) -> <return type>:
+#               <Base Case>
+#
+#               <Recursive Case>
+#
+# Create a that takes one non negative integer.
+# Return the fibonacci of that number.
+# Return -1 if the input is negative.
+# Ex:
+#   fibonacci(10) # return 55
+#   fibonacci(12) # return 144
