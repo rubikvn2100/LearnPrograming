ab99fc91ee0e654c4728bc1ceb339ef50048ecc0
Khoa Le <rubikvn2100@gmail.com>
2023-05-02 00:14:09 -0700

    Add "countUnorderedAdditionOfSum.py" problem statement.

---
+++ recursion/naiveImplementation/countUnorderedAdditionOfSum.py
@@ -0,0 +1,16 @@
+# Create a that takes one integer sum.
+# Return the number of unordered list of non negative integer that add up to sum.
+# Ex:
+#   countUnorderedAdditionOfSum(4) # return 8
+#   countUnorderedAdditionOfSum(9) # return 256
+#
+# Explanation:
+#   When sum = 4, the list that have sum add up to 4 unordered are:
+#     4 = 1 + 1 + 1 + 1
+#     4 = 1 + 1 + 2
+#     4 = 1 + 2 + 1
+#     4 = 1 + 3
+#     4 = 2 + 1 + 1
+#     4 = 2 + 2
+#     4 = 3 + 1
+#     4 = 4
