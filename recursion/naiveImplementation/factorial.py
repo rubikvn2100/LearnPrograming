3f5fda5a44157cae500ed638cbf1b01c6852e2e2
Khoa Le <rubikvn2100@gmail.com>
2023-05-02 00:10:33 -0700

    Add "factorial.py" problem statement.

---
+++ recursion/naiveImplementation/factorial.py
@@ -0,0 +1,14 @@
+# Note: Recursion is a programming technique where a function calls itself to
+#       solve a problem or perform a task. Recursion use to have the following structure:
+#
+#           def recursionFunction(<parameters>) -> <return type>:
+#               <Base Case>
+#
+#               <Recursive Case>
+#
+# Create a that takes one non negative integer.
+# Return the factorial of that number.
+# Return -1 if the input is negative.
+# Ex:
+#   factorial(5) # return 120
+#   factorial(6) # return 720
